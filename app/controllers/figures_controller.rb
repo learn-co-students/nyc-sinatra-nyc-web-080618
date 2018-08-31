710029e2dcbbb28b712b9f1e0bd74fcc98c30f62
kristinatong <kristina.tong@gmail.com>
2018-08-31 10:35:24 -0400

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,51 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if params[:title][:name] != ""
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    end
+    if params[:landmark][:name] != ""
+      @landmark = Landmark.create(name:params[:landmark][:name])
+      @figure.landmarks << @landmark
+    end
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/edit'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    if params[:title][:name] != ""
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    end
+    if params[:landmark][:name] != ""
+      @landmark = Landmark.create(name:params[:landmark][:name])
+      @figure.landmarks << @landmark
+    end
+    redirect "/figures/#{@figure.id}"
+  end
+
 end
